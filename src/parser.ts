02fc330648353ca2af3a993a61e38100b15e892a
ewfian <ewfian@vip.qq.com>
2023-04-15 22:51:42 +0900

    re-organising opcodes in parser

--- src/parser.ts
+++ src/parser.ts
@@ -21,6 +21,7 @@ export class Parser {
             const opcode = reader.byte();
             // console.log(`${(reader.position - 1).toString()} ${opcode}`);
             switch (opcode) {
+                // Structural
                 case OP.PROTO: {
                     const version = reader.byte();
                     if (version > 4) {
@@ -28,61 +29,203 @@ export class Parser {
                     }
                     break;
                 }
+                case OP.STOP:
+                    return stack.pop();
                 case OP.FRAME:
+                    // Ignore framing, but still have to gobble up the length.
                     reader.skip(8);
-                    break;
-                case OP.SHORT_BINUNICODE:
-                    stack.push(reader.string(reader.byte(), 'utf-8'));
-                    break;
-                case OP.MEMOIZE:
-                    memo.set(memo.size, stack[stack.length - 1]);
-                    break;
-                case OP.STACK_GLOBAL: {
-                    const name = stack.pop();
-                    const module = stack.pop();
-                    const cls = this.resolveClass(module, name);
-                    stack.push(cls);
-                    break;
-                }
-                case OP.GLOBAL: {
-                    const module = reader.line();
-                    const name = reader.line();
-                    const cls = this.resolveClass(module, name);
-                    stack.push(cls);
-                    break;
-                }
-                case OP.EMPTY_TUPLE:
-                    stack.push([]);
-                    break;
-                case OP.NEWOBJ: {
-                    const args = stack.pop();
-                    const cls = stack.pop();
-                    const obj = this.newObject(cls, ...args);
-                    stack.push(obj);
-                    break;
-                }
-                case OP.EMPTY_DICT:
-                    stack.push({});
                     break;
                 case OP.MARK:
                     metastack.push(stack);
                     stack = [];
                     break;
-                case OP.BINUNICODE:
-                    stack.push(reader.string(reader.uint32(), 'utf-8'));
+                case OP.POP_MARK:
+                    stack = metastack.pop();
                     break;
+                case OP.POP:
+                    stack.pop();
+                    break;
+                case OP.DUP:
+                    stack.push(stack[stack.length - 1]);
+                    break;
+
+                // Memo saving
+                case OP.PUT: {
+                    const index = parseInt(reader.line(), 10);
+                    memo.set(index, stack[stack.length - 1]);
+                    break;
+                }
+                case OP.BINPUT:
+                    memo.set(reader.byte(), stack[stack.length - 1]);
+                    break;
+                case OP.LONG_BINPUT:
+                    memo.set(reader.uint32(), stack[stack.length - 1]);
+                    break;
+                case OP.MEMOIZE:
+                    memo.set(memo.size, stack[stack.length - 1]);
+                    break;
+
+                // Memo getting
+                case OP.GET: {
+                    const index = parseInt(reader.line(), 10);
+                    stack.push(memo.get(index));
+                    break;
+                }
+                case OP.BINGET:
+                    stack.push(memo.get(reader.byte()));
+                    break;
+                case OP.LONG_BINGET:
+                    stack.push(memo.get(reader.uint32()));
+                    break;
+
+                // Literal
+                case OP.NONE:
+                    stack.push(null);
+                    break;
+                case OP.NEWTRUE:
+                    stack.push(true);
+                    break;
+                case OP.NEWFALSE:
+                    stack.push(false);
+                    break;
+
+                // ASCII-formatted numbers
+                case OP.INT: {
+                    const value = reader.line();
+                    if (value == '01') {
+                        stack.push(true);
+                    } else if (value == '00') {
+                        stack.push(false);
+                    } else {
+                        stack.push(parseInt(value, 10));
+                    }
+                    break;
+                }
+                case OP.LONG:
+                    stack.push(parseInt(reader.line(), 10));
+                    break;
+                case OP.FLOAT:
+                    stack.push(parseFloat(reader.line()));
+                    break;
+
+                // ASCII-formatted strings
                 case OP.STRING: {
                     const str = reader.line();
                     stack.push(str.substr(1, str.length - 2));
                     break;
                 }
-                case OP.BINSTRING:
-                case OP.SHORT_BINSTRING:
-                    stack.push(reader.string(reader.uint32(), 'ascii'));
-                    break;
                 case OP.UNICODE:
                     stack.push(reader.line());
                     break;
+
+                // Binary-coded numbers
+                case OP.BININT:
+                    stack.push(reader.int32());
+                    break;
+                case OP.BININT1:
+                    stack.push(reader.byte());
+                    break;
+                case OP.BININT2:
+                    stack.push(reader.uint16());
+                    break;
+                case OP.LONG1: {
+                    const length = reader.byte();
+                    const data = reader.bytes(length);
+                    const number = this.readUint64(data);
+                    stack.push(number);
+                    break;
+                }
+                // case OP.LONG4:
+                //     break;
+                case OP.BINFLOAT:
+                    stack.push(reader.float64());
+                    break;
+
+                // Length-prefixed (byte)strings
+                case OP.BINBYTES:
+                    stack.push(reader.bytes(reader.int32()));
+                    break;
+                case OP.SHORT_BINBYTES:
+                    stack.push(reader.bytes(reader.byte()));
+                    break;
+                case OP.BINSTRING:
+                    stack.push(reader.string(reader.uint32(), 'ascii'));
+                    break;
+                case OP.SHORT_BINSTRING:
+                    stack.push(reader.string(reader.byte(), 'ascii'));
+                    break;
+                case OP.BINUNICODE:
+                    stack.push(reader.string(reader.uint32(), 'utf-8'));
+                    break;
+                case OP.SHORT_BINUNICODE:
+                    stack.push(reader.string(reader.byte(), 'utf-8'));
+                    break;
+
+                // Tuples
+                case OP.EMPTY_TUPLE:
+                    stack.push([]);
+                    break;
+                case OP.TUPLE: {
+                    const items = stack;
+                    stack = metastack.pop();
+                    stack.push(items);
+                    break;
+                }
+                case OP.TUPLE1: {
+                    stack.push([stack.pop()]);
+                    break;
+                }
+                case OP.TUPLE2: {
+                    const b = stack.pop();
+                    const a = stack.pop();
+                    stack.push([a, b]);
+                    break;
+                }
+                case OP.TUPLE3: {
+                    const c = stack.pop();
+                    const b = stack.pop();
+                    const a = stack.pop();
+                    stack.push([a, b, c]);
+                    break;
+                }
+
+                // Lists
+                case OP.EMPTY_LIST:
+                    stack.push([]);
+                    break;
+                case OP.LIST: {
+                    const items = stack;
+                    stack = metastack.pop();
+                    stack.push(items);
+                    break;
+                }
+                case OP.APPEND: {
+                    const append = stack.pop();
+                    stack[stack.length - 1].push(append);
+                    break;
+                }
+                case OP.APPENDS: {
+                    const appends = stack;
+                    stack = metastack.pop();
+                    const list = stack[stack.length - 1];
+                    list.push(...appends);
+                    break;
+                }
+
+                // Dicts
+                case OP.EMPTY_DICT:
+                    stack.push({});
+                    break;
+                case OP.DICT: {
+                    const items = stack;
+                    stack = metastack.pop();
+                    const dict: any = {};
+                    for (let i = 0; i < items.length; i += 2) {
+                        dict[items[i]] = items[i + 1];
+                    }
+                    stack.push(dict);
+                    break;
+                }
                 case OP.SETITEM: {
                     const value = stack.pop();
                     const key = stack.pop();
@@ -108,36 +251,9 @@ export class Parser {
                     }
                     break;
                 }
-                case OP.INT: {
-                    const value = reader.line();
-                    if (value == '01') {
-                        stack.push(true);
-                    } else if (value == '00') {
-                        stack.push(false);
-                    } else {
-                        stack.push(parseInt(value, 10));
-                    }
-                    break;
-                }
-                case OP.BININT:
-                    stack.push(reader.int32());
-                    break;
-                case OP.BININT1:
-                    stack.push(reader.byte());
-                    break;
-                case OP.BININT2:
-                    stack.push(reader.uint16());
-                    break;
-                case OP.BINBYTES:
-                    stack.push(reader.bytes(reader.int32()));
-                    break;
-                case OP.SHORT_BINBYTES:
-                    stack.push(reader.bytes(reader.byte()));
-                    break;
+
+                // Sets
                 case OP.EMPTY_SET:
-                    stack.push([]);
-                    break;
-                case OP.EMPTY_LIST:
                     stack.push([]);
                     break;
                 case OP.FROZENSET: {
@@ -146,25 +262,6 @@ export class Parser {
                     stack.push(items);
                     break;
                 }
-                case OP.DICT: {
-                    const items = stack;
-                    stack = metastack.pop();
-                    const dict: any = {};
-                    for (let i = 0; i < items.length; i += 2) {
-                        dict[items[i]] = items[i + 1];
-                    }
-                    stack.push(dict);
-                    break;
-                }
-                case OP.LIST: {
-                    const items = stack;
-                    stack = metastack.pop();
-                    stack.push(items);
-                    break;
-                }
-                case OP.BINGET:
-                    stack.push(memo.get(reader.byte()));
-                    break;
                 case OP.ADDITEMS: {
                     const items = stack;
                     stack = metastack.pop();
@@ -172,6 +269,31 @@ export class Parser {
                     for (let i = 0; i < items.length; i++) {
                         obj.push(items[i]);
                     }
+                    break;
+                }
+
+                //  Module globals
+                case OP.GLOBAL: {
+                    const module = reader.line();
+                    const name = reader.line();
+                    const cls = this.resolveClass(module, name);
+                    stack.push(cls);
+                    break;
+                }
+                case OP.STACK_GLOBAL: {
+                    const name = stack.pop();
+                    const module = stack.pop();
+                    const cls = this.resolveClass(module, name);
+                    stack.push(cls);
+                    break;
+                }
+
+                // Classes
+                case OP.NEWOBJ: {
+                    const args = stack.pop();
+                    const cls = stack.pop();
+                    const obj = this.newObject(cls, ...args);
+                    stack.push(obj);
                     break;
                 }
                 case OP.BUILD: {
@@ -194,96 +316,7 @@ export class Parser {
                     }
                     break;
                 }
-                case OP.LONG_BINGET:
-                    stack.push(memo.get(reader.uint32()));
-                    break;
-                case OP.TUPLE: {
-                    const items = stack;
-                    stack = metastack.pop();
-                    stack.push(items);
-                    break;
-                }
-                case OP.TUPLE1: {
-                    stack.push([stack.pop()]);
-                    break;
-                }
-                case OP.TUPLE2: {
-                    const b = stack.pop();
-                    const a = stack.pop();
-                    stack.push([a, b]);
-                    break;
-                }
-                case OP.TUPLE3: {
-                    const c = stack.pop();
-                    const b = stack.pop();
-                    const a = stack.pop();
-                    stack.push([a, b, c]);
-                    break;
-                }
-                case OP.NONE:
-                    stack.push(null);
-                    break;
-                case OP.FLOAT:
-                    stack.push(parseFloat(reader.line()));
-                    break;
-                case OP.BINFLOAT:
-                    stack.push(reader.float64());
-                    break;
-                case OP.NEWTRUE:
-                    stack.push(true);
-                    break;
-                case OP.NEWFALSE:
-                    stack.push(false);
-                    break;
-                case OP.LONG:
-                    stack.push(parseInt(reader.line(), 10));
-                    break;
-                case OP.LONG1: {
-                    const length = reader.byte();
-                    const data = reader.bytes(length);
-                    const number = this.readUint64(data);
-                    stack.push(number);
-                    break;
-                }
-                case OP.POP:
-                    stack.pop();
-                    break;
-                case OP.POP_MARK:
-                    stack = metastack.pop();
-                    break;
-                case OP.DUP:
-                    stack.push(stack[stack.length - 1]);
-                    break;
-                case OP.PUT: {
-                    const index = parseInt(reader.line(), 10);
-                    memo.set(index, stack[stack.length - 1]);
-                    break;
-                }
-                case OP.APPEND: {
-                    const append = stack.pop();
-                    stack[stack.length - 1].push(append);
-                    break;
-                }
-                case OP.APPENDS: {
-                    const appends = stack;
-                    stack = metastack.pop();
-                    const list = stack[stack.length - 1];
-                    list.push(...appends);
-                    break;
-                }
-                case OP.BINPUT:
-                    memo.set(reader.byte(), stack[stack.length - 1]);
-                    break;
-                case OP.LONG_BINPUT:
-                    memo.set(reader.uint32(), stack[stack.length - 1]);
-                    break;
-                case OP.GET: {
-                    const index = parseInt(reader.line(), 10);
-                    stack.push(memo.get(index));
-                    break;
-                }
-                case OP.STOP:
-                    return stack.pop();
+
                 default:
                     // console.log('metastack:', metastack, '\nstack:', stack);
                     // console.log('\nmemo:', Array.from(memo.entries()));
