a749ba0a15a03fe407b8ebb684949f358fcb0849
ewfian <ewfian@vip.qq.com>
2023-04-24 21:36:30 +0900

    fix ut failed

--- test/Parser.test.ts
+++ test/Parser.test.ts
@@ -4,13 +4,14 @@ import { Parser } from '../src/parser';
 describe('Parser', () => {
     describe('#load', () => {
         it('throws an error if the input buffer is empty', () => {
-            const parser = new Parser(new Uint8Array());
-            expect(() => parser.load()).toThrow('Unexpected end of file.');
+            const parser = new Parser();
+            expect(() => parser.parse(new Uint8Array())).toThrow('Unexpected end of file.');
         });
 
         it('correctly load data', () => {
-            const parser = new Parser(new Uint8Array([OP.PROTO, 4, OP.BININT1, 3, OP.STOP]));
-            expect(parser.load()).toEqual(3);
+            const parser = new Parser();
+            const pkl = new Uint8Array([OP.PROTO, 4, OP.BININT1, 3, OP.STOP]);
+            expect(parser.parse(pkl)).toEqual(3);
         });
     });
 });
